a9da32a76ced8c3e9967433a599d636273fd7faf
zohra101 <alex.marjanovic.ba.tw@gmail.com>
2025-04-01 22:22:22 -0500

    can't remember what changes I made because interrupted

--- backend/routes/api.ts
+++ backend/routes/api.ts
@@ -2,7 +2,6 @@ import { Request, Response } from "express";
 
 //function handler() {}; //Declare the handler as an empty function
 export function api(request: Request, response: Response) {
-    // const data = ({ message: "hello"}) ;
     const data = { origin: request.headers.origin,
         query: request.query,
      };
--- backend/routes/root.ts
+++ backend/routes/root.ts
@@ -3,7 +3,7 @@ import { Request, Response } from "express";
 //function handler() {}; //Declare the handler as an empty function
 export function root(request: Request, response: Response) {
     const { url } = request;
-    response.send(`<h1>Express Template< /h1><p>Backend root page.</p>`);
+    response.send(`<h1>Capstone Level 4 Express</h1><p>Backend root page.</p>`);
     debugger;
 
     // response.send({ message: "Hello world!"});
