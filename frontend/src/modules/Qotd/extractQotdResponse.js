77eb41bb483eef8cd6864e122a845629bf80d150
zohra101 <alex.marjanovic.ba.tw@gmail.com>
2025-04-03 21:37:44 -0500

    Removed js files that somehow did not get overwritten in the renaming

--- frontend/src/modules/Qotd/extractQotdResponse.js
+++
@@ -1,8 +0,0 @@
-//Extract the server resoponse and parse it
-import { parseQotdResponse } from "./parseQotdResponse";
-
-export function extractQotdResponse(resolveValue: any) {
-	const promise = resolveValue.text();
-	promise.then(parseQotdResponse);
-	console.log("Quote of the Day was extracted.");
-}
--- frontend/src/modules/consultation/parseResponseConsultation.js
+++
@@ -1,9 +0,0 @@
- //Form processing messages
-//  const emailSubmitMessage = "<br>" + "<h5>Your message has been sent from" + email + ".</h5>" + "<br>";
- // const scheduleSubmitMessage = "<br>" + "<h5>Your consultation request has been submitted. Confirmation will be sent to" + email + ".</h5>" + "<br>";
-
-export function parseResponseConsultation(resolveValue2) {
-    const response = JSON.parse(resolveValue2);
-    const message = response.message;
-    output2(message);
-}
