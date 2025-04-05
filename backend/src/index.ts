585af183a98979a066997be1d44b29d51837c326
zohra101 <alex.marjanovic.ba.tw@gmail.com>
2025-04-05 00:04:38 -0500

    Created TestBackend component and updated the api route on the backend to use this new component

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import { root } from "./routes/root";
+import { backend } from "./routes/backend";
 
 const hostname = "localhost"; //Local domainnpm
 const port = 9000; //Common backend ports are 3000, 8080, 9000
@@ -9,6 +10,7 @@ const path = "/"; //The path from where the server info will be rendered in the 
 const app = express(); //Instantiate an express.js object
 app.use(cors());
 app.get("/", root); //The handler runs when the path is visited in the URL
+app.get("/backend", backend); //The handler runs when the path is visited in the URL
 app.listen(port, hostname, handleListen); //Listen on the specified port and hostname
 
 
