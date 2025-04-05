aadf3b26488884835e9b1e2d2632753dfb132fdb
zohra101 <alex.marjanovic.ba.tw@gmail.com>
2025-04-04 23:49:01 -0500

    Captsone backend set up completed

--- backend/routes/api.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from "express";
-
-//function handler() {}; //Declare the handler as an empty function
-export function api(request: Request, response: Response) {
-    const data = { origin: request.headers.origin,
-        query: request.query,
-     };
-    response.send(data);
-}
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,6 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import { root } from "./routes/root";
-import { api } from "./routes/api";
 
 const hostname = "localhost"; //Local domainnpm
 const port = 9000; //Common backend ports are 3000, 8080, 9000
@@ -10,7 +9,6 @@ const path = "/"; //The path from where the server info will be rendered in the 
 const app = express(); //Instantiate an express.js object
 app.use(cors());
 app.get("/", root); //The handler runs when the path is visited in the URL
-app.get("/api", api); //The handler runs when the path is visited in the URL
 app.listen(port, hostname, handleListen); //Listen on the specified port and hostname
 
 
