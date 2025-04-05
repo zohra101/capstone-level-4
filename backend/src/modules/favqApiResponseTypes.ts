0b534b0ff8900f8d0a88010e4faf51c38ac149e1
zohra101 <alex.marjanovic.ba.tw@gmail.com>
2025-04-05 13:25:18 -0500

    create and use a branch for defining a custom datatype for the API response

---
+++ backend/src/modules/favqApiResponseTypes.ts
@@ -0,0 +1,11 @@
+//Define the custom datatype for the return value of the API function
+export type FavqApiResponse = {
+  quote: string;
+ author: string;
+};
+
+//Assert the datatype
+
+
+//Assert the function return type
+
---
+++ backend/src/modules/getFavqApiResponse.ts
@@ -0,0 +1,5 @@
+import { ApiResponse } from "./favqApiResponseTypes";
+
+export function getApiResponse(): ApiResponse {
+
+}
