12b44fe40cac8879d3d56d8184255a010f788fc7
zohra101 <alex.marjanovic.ba.tw@gmail.com>
2025-04-18 15:30:29 -0500

    Coded fronted showing DyamoDB auth response in Footer

--- frontend/src/Views/nav/Footer.tsx
+++ frontend/src/Views/nav/Footer.tsx
@@ -1,24 +1,28 @@
 import React from "react";
 import copyright from "../../../assets/icons/copyright_cGainsboro_nobg.png";
 import { TestBackend } from "../TestBackend";
+import { AuthenticateDynamoDB } from "../AuthenticateDynamoDB";
 
 export function Footer() {
 	return (
-			<footer>
-				<hr />
-				<div className="m-2">
-					{" "}
-					2025{" "}
-					<img
-						id="copyright"
-						width="15px"
-						src={copyright}
-					/>{" "}
-					Aleksandra Marjanovic
-				</div>
-				<div>
-					<TestBackend/>
-				</div>
-			</footer>
+		<footer>
+			<hr />
+			<div className="m-2">
+				{" "}
+				2025{" "}
+				<img
+					id="copyright"
+					width="15px"
+					src={copyright}
+				/>{" "}
+				Aleksandra Marjanovic
+			</div>
+			<div>
+				<TestBackend />
+			</div>
+			<div>
+				<AuthenticateDynamoDB/>
+			</div>
+		</footer>
 	);
 }
