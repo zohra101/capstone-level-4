a4648ae28cce436354affb5c515f0594306b144f
zohra101 <alex.marjanovic.ba.tw@gmail.com>
2025-04-30 13:28:58 -0500

    readAccount test and function

---
+++ backend/src/modules/dynamoDB/readAccount.ts
@@ -0,0 +1,40 @@
+import { DynamoDB } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocument, GetCommandInput } from "@aws-sdk/lib-dynamodb";
+import dotenv from "dotenv";
+
+
+dotenv.config(); //Attaches the env variables in .env to the process object
+
+export async function readAccount(targetEmail: string, targetPassword: string): Promise<UserAccount | undefined> {
+	if (typeof targetEmail === "object") return undefined;
+	if (targetEmail === undefined || targetPassword === undefined) return undefined;
+
+	const apiKey = {
+		region: process.env.region,
+		credentials: {
+			accessKeyId: process.env.accessKeyId,
+			secretAccessKey: process.env.secretAccessKey,
+		},
+	};
+
+
+
+	//Connect to DynamoDB
+	const client = new DynamoDB(apiKey);
+	const niceClient = DynamoDBDocument.from(client);
+
+	const request: GetCommandInput = {
+		TableName: "logins",
+		Key: { email: targetEmail },
+	};
+
+
+	const response = await niceClient.get(request);
+	let userAccount = response.Item as UserAccount;
+	if (userAccount && userAccount.password !== targetPassword)
+		userAccount = undefined;
+
+	return userAccount;
+}
+
+
