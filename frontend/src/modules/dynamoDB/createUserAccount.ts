6807eaff1946a5b9e74e3adc491f72d877f946bc
zohra101 <alex.marjanovic.ba.tw@gmail.com>
2025-05-13 14:20:37 -0500

    readAccount tests and function passed

--- frontend/src/modules/dynamoDB/createUserAccount.ts
+++ frontend/src/modules/dynamoDB/createUserAccount.ts
@@ -5,8 +5,15 @@ const backendURL = "http://localhost:9000";
 const backendRoute = "/createAccount";
 
 export async function createUserAccount(userAccount: UserAccount) {
-	const { email, password } = userAccount;
-    const url = `${backendURL}${backendRoute}?email=${email}&password=${password}`;
+	console.log("createUserAccount called with:", userAccount);
+	console.log("Email to validate:", userAccount.email);
+
+	if (userAccount.password === null) {
+		return "A password is required to create an account. Please enter a valid password.";
+	}
+
+	const emailToSend = userAccount.email === null ? "" : userAccount.email;
+    const url = `${backendURL}${backendRoute}?email=${emailToSend}&password=${userAccount.password}`;
 
 	const response = await axios.put(url);
 	return response.data;
