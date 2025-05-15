69da361d64a3efb2d0569f55033b46c018c035a5
zohra101 <alex.marjanovic.ba.tw@gmail.com>
2025-05-15 13:05:27 -0500

    commited new build

---
+++ frontend/src/modules/state/stateSelectors.ts
@@ -0,0 +1,35 @@
+import { StateVariables } from "./stateVariables";
+
+
+export function selectDidMount(state: StateVariables) {
+	const { didMount } = state;
+	return didMount;
+}
+
+export function selectComponent(state: StateVariables) {
+	const { component } = state;
+	return component;
+}
+
+export function selectIsAuthenticated(state: StateVariables) {
+	const { isAuthenticated } = state;
+	return isAuthenticated;
+}
+
+export function selectIsSignedIn(state: StateVariables) {
+	const { isSignedIn } = state;
+	return isSignedIn;
+}
+
+export function selectIsSignedOut(state: StateVariables) {
+	const { isSignedOut } = state;
+	return isSignedOut;
+}
+
+export function selectIsActiveUser(state: StateVariables) {
+	const { isActiveUser } = state;
+	return isActiveUser;
+}
+
+
+
