24d214db8498a70b47f2bb7d9cf5c37230e9f598
zohra101 <alex.marjanovic.ba.tw@gmail.com>
2025-05-17 10:46:47 -0500

    commited new build

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,20 +1,20 @@
 import { createRoot } from "react-dom/client";
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router";
+// import { BrowserRouter, Routes, Route } from "react-router";
 import "../src/index.scss";
-import { Home } from "../src/Views/home/Home";
-import { About } from "../src/Views/About";
-import { AlexResume } from "../src/Views/AlexResume";
-import { Portfolio } from "../src/Views/Portfolio";
+// import { Home } from "../src/Views/home/Home";
+// import { About } from "../src/Views/About";
+// import { AlexResume } from "../src/Views/AlexResume";
+// import { Portfolio } from "../src/Views/Portfolio";
 import { getRootPath } from "../src/utils/getRootPath";
-import { Header } from "../src/Views/nav/Header";
-import { Footer } from "../src/Views/nav/Footer";
-import { HandleRefresh } from "../src/Views/nav/HandleRefresh";
-import { Consultation } from "./Views/Consultation";
-import { Message } from "./Views/Message";
-import { Account } from "./Views/Account";
-import { Provider } from "react-redux";
-import { store } from "./modules/state/store";
+// import { Header } from "../src/Views/nav/Header";
+// import { Footer } from "../src/Views/nav/Footer";
+// import { HandleRefresh } from "../src/Views/nav/HandleRefresh";
+// import { Consultation } from "./Views/Consultation";
+// import { Message } from "./Views/Message";
+// import { Account } from "./Views/Account";
+// import { Provider } from "react-redux";
+// import { store } from "./modules/state/store";
 
 
 const bodyTag = document.getElementById("bodyTag");
@@ -22,42 +22,43 @@ const root = createRoot(bodyTag);
 const rootPath = getRootPath();
 
 root.render(
-	<Provider store={store}>
-		<BrowserRouter>
-			<HandleRefresh>
-				<Header />
-				<Routes>
-					<Route
-						path={`${rootPath}/`}
-						element={<Home />}
-					/>
-					<Route
-						path={`${rootPath}/portfolio`}
-						element={<Portfolio />}
-					/>
-					<Route
-						path={`${rootPath}/alexresume`}
-						element={<AlexResume />}
-					/>
-					<Route
-						path={`${rootPath}/contact`}
-						element={<Message />}
-					/>
-					<Route
-						path={`${rootPath}/schedulefreeconsultation`}
-						element={<Consultation />}
-					/>
-					<Route
-						path={`${rootPath}/account`}
-						element={<Account />}
-					/>
-					<Route
-						path={`${rootPath}/about`}
-						element={<About />}
-					/>
-				</Routes>
-				<Footer />,
-			</HandleRefresh>
-		</BrowserRouter>
-	</Provider>
+	<h1>Hello World</h1>
+	// <Provider store={store}>
+	// 	<BrowserRouter>
+	// 		<HandleRefresh>
+	// 			<Header />
+	// 			<Routes>
+	// 				<Route
+	// 					path={`${rootPath}/`}
+	// 					element={<Home />}
+	// 				/>
+	// 				<Route
+	// 					path={`${rootPath}/portfolio`}
+	// 					element={<Portfolio />}
+	// 				/>
+	// 				<Route
+	// 					path={`${rootPath}/alexresume`}
+	// 					element={<AlexResume />}
+	// 				/>
+	// 				<Route
+	// 					path={`${rootPath}/contact`}
+	// 					element={<Message />}
+	// 				/>
+	// 				<Route
+	// 					path={`${rootPath}/schedulefreeconsultation`}
+	// 					element={<Consultation />}
+	// 				/>
+	// 				<Route
+	// 					path={`${rootPath}/account`}
+	// 					element={<Account />}
+	// 				/>
+	// 				<Route
+	// 					path={`${rootPath}/about`}
+	// 					element={<About />}
+	// 				/>
+	// 			</Routes>
+	// 			<Footer />,
+	// 		</HandleRefresh>
+	// 	</BrowserRouter>
+	// </Provider>
 );
