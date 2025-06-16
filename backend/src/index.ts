fc73c56fc1f0aeaaac60be651aad674bbfef154c
zohra101 <alex.marjanovic.ba.tw@gmail.com>
2025-06-15 22:35:36 -0500

    Converted readAccount to POST

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -28,14 +28,14 @@ app.get("/", root); //The handler runs when the path is visited in the URL
 app.get("/favqApiResponse", favqApiResponse); //The handler runs when the path is visited in the URL
 
 app.get("/createAccount", createAccount); //The handler runs when the path is visited in the URL
-app.get("/readAccount", readAccount); //The handler runs when the path is visited in the URL
+// app.get("/readAccount", readAccount); //The handler runs when the path is visited in the URL
 app.get("/updateAccount", updateAccount); //The handler runs when the path is visited in the URL
 app.get("/delAccount", delAccount); //The handler runs when the path is visited in the URL
 
 // app.post("/test", (req, res) => {res.send("Test handler is working");});
 app.post("/authenticateAws", authenticateAws); //The handler runs when the path is visited in the URL
 // app.post("/createAccount", createAccount); //The handler runs when the path is visited in the URL
-// app.post("/readAccount", readAccount); //The handler runs when the path is visited in the URL
+app.post("/readAccount", readAccount); //The handler runs when the path is visited in the URL
 // app.post("/updateAccount", updateAccount); //The handler runs when the path is visited in the URL
 // app.post("/delAccount", delAccount); //The handler runs when the path is visited in the URL
 
--- backend/src/routes/readAccount.ts
+++ backend/src/routes/readAccount.ts
@@ -6,7 +6,8 @@ import { UserAccount } from "../modules/dynamoDB/UserAccount";
 //function handler() {}; //Declare the handler as an empty function
 export async function readAccount(request: Request, response: Response) {
 	//Extract user account data from the request
-	const userAccount: UserAccount = request.query as any;
+	// const userAccount: UserAccount = request.query as any;
+	const userAccount: UserAccount = request.body as any;
 
 	//Call readUserAccount with the user data
 	const result = await readUserAccount(userAccount as UserAccount);
